24482dc0f22d2d4a7c9905666defbab38bc6a2f2
Emil Nikolov <emil.e.nikolov@gmail.com>
2022-10-30 17:17:35 +0100

    zz

---
+++ default.nix
@@ -0,0 +1,38 @@
+# # Shell for bootstrapping flake-enabled nix and home-manager
+{ pkgs ? let
+    # If pkgs is not defined, instantiate nixpkgs from locked commit
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+    system = builtins.currentSystem;
+    overlays = [ ]; # Explicit blank overlay to avoid interference
+  in
+  import nixpkgs { inherit system overlays; }
+, ...
+}: pkgs.mkShell
+{
+  # Enable experimental features without having to specify the argument
+  NIX_CONFIG = "experimental-features = nix-command flakes";
+  nativeBuildInputs = with pkgs; [
+    nix
+    home-manager
+    git
+
+    sops
+    gnupg
+    age
+
+    zsh
+  ];
+
+  shellHook = ''
+    exec zsh
+  '';
+}
+
+# (import (fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
+#   src = ./.;
+#   system = builtins.currentSystem;
+# }).shellNix
