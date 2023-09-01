59713ff82eb4099b17a21527cc7c7ea934fee55f
e-nikolov <emil.e.nikolov@gmail.com>
2023-09-01 09:23:17 +0200

    flake update

--- hosts/minimal/home.nix
+++ hosts/minimal/home.nix
@@ -60,7 +60,8 @@
   programs.zoxide.enableZshIntegration = true;
   programs.zellij.enable = true;
   programs.zellij.settings = {
-    copy_on_select = false;
+    # copy_on_select = false;
+    default_layout = "compact";
 
     keybinds = {
       unbind = [ "Alt Left" "Alt Right" "Ctrl 1" "Ctrl 3" "Ctrl 5" "Ctrl 2" ];
@@ -69,6 +70,8 @@
         "bind \"Ctrl 3\"" = { MoveFocusOrTab = "Right"; };
         "bind \"Ctrl 5\"" = { MoveFocus = "Down"; };
         "bind \"Ctrl 2\"" = { MoveFocus = "Up"; };
+        "bind \"Ctrl e\"" = { SwitchToMode = "Tab"; };
+        "bind \"Alt t\"" = { NewTab = ""; };
       };
     };
   };
