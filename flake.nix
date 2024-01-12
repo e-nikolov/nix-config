25c2768a7198fb70d79d545c9128cb5762b097b9
e-nikolov <emil.e.nikolov@gmail.com>
2024-01-12 13:45:53 +0100

    auto-upgrade

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
     ...
   }: let
     personal-info = {
-      flake-path = "~/nix-config/";
       username = "enikolov";
       gitUsername = "e-nikolov";
       email = "emil.e.nikolov@gmail.com";
+      flake-path = "/home/${personal-info.username}/nix-configs";
     };
     default-module = {pkgs, ...}: {
       nix.package = pkgs.nixFlakes;
