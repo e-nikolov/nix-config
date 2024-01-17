c823abd82340932813badc369c4a0d4952eed397
e-nikolov <emil.e.nikolov@gmail.com>
2024-01-17 05:48:15 +0100

    helix

--- hosts/home-nix/home.nix
+++ hosts/home-nix/home.nix
@@ -1,33 +1,3 @@
-{ config, pkgs, inputs, self, ... }:
-let
-  # _1password_desktopItem = pkgs.makeDesktopItem {
-  #   name = "_1password-gui";
-  #   exec = "_1password-gui";
-  #   icon = "_1password-gui";
-  #   # comment = meta.description;
-  #   # genericName = meta.description;
-  #   type = "Application";
-  #   desktopName = "1Password";
-  #   categories = [ "Video" "AudioVideo" ];
-  # };
-  _1password_autostart = pkgs.makeAutostartItem {
-    name = "1password";
-    package = pkgs._1password-gui;
-  };
-in {
+{ config, pkgs, inputs, self, ... }: {
   imports = [ ../../presets/base/home.nix ../../presets/wsl/home.nix ];
-
-  systemd.user.services = {
-    _1password_gui_autostart = {
-      Unit = { Description = "1Password GUI Autostart"; };
-
-      Service = {
-        Environment = "DISPLAY=:0";
-        ExecStartPre = "${pkgs.coreutils-full}/bin/chmod 700 /run/user/1000";
-        ExecStart = "${pkgs._1password-gui}/bin/1password";
-        Restart = "always";
-      };
-      Install.WantedBy = [ "default.target" ];
-    };
-  };
 }
--- presets/base/configuration.nix
+++ presets/base/configuration.nix
@@ -69,24 +69,6 @@ in {
         test "$(lastModified "${config.system.autoUpgrade.flake}")"  -gt "$(lastModified "${personal-info.flake-path}")"
       '');
   };
-  programs._1password.enable = true;
-  programs._1password-gui.enable = true;
-  programs._1password-gui.polkitPolicyOwners = [ personal-info.username ];
-
-  # programs._1password-gui.package = (pkgs._1password-gui-beta.overrideAttrs
-  #   (oldAttrs: {
-  #     postInstall = ''
-  #       ln -s $out/share/1password/op-ssh-sign $out/bin/op-ssh-sign
-  #     '';
-  #   }));
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = false;
-  # };
 
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
 
